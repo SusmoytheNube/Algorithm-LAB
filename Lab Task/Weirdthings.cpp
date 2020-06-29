6e38a32e23c2bfbda8e843fac59d8a7429b7f06f
Susmoy Paul <61885664+susmoy15-12910@users.noreply.github.com>
2020-06-29 14:00:09 +0600

    Create Weirdthings.cpp

---
+++ Lab Task/Weirdthings.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N,arr[MAX_PATH];
+
+     cout<< "Enter Array size =";
+     cin>> N;
+     cout<<endl;
+     for (i=0;i<n;i++)
+        cin>> arr[i];
+    
+
+}
