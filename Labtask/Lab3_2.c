1f14d00f8386719e0603713e81bf9edfc357aa8c
Susmoy Paul <61885664+susmoy15-12910@users.noreply.github.com>
2020-08-04 13:35:31 +0600

    Create Lab3_2.c

---
+++ Labtask/Lab3_2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+ int i,n,key,arr[1000],a;
+ printf("Enter Array Size : ");
+ scanf("%d",&n);
+ for(i=0; i<n; i++)
+ {
+ scanf("%d",&arr[i]);
+ }
+ printf("Enter Search key : ");
+ scanf("%d",&key);
+ for(i=0; i<n; i++)
+ {
+ if(arr[i]==key)
+ {
+     a=i;
+ }
+ }
+ if(a!= NULL)
+ {
+    printf("Position = %d Row",a);
+ }
+ else
+ {
+     printf("Product not available ");
+ } 
+ return 0;
+}
+ 
