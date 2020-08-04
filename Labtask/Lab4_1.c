d8dbd6b4730c5e232d465805cd804d8fe79979c0
Susmoy Paul <61885664+susmoy15-12910@users.noreply.github.com>
2020-08-04 13:35:38 +0600

    Create Lab4_1.c

---
+++ Labtask/Lab4_1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+ int n,i,j, temp;
+ printf("Enter Array Size : ");
+ scanf("%d",&n);
+ int a[n];
+ printf("Enter Elements of Array : \n");
+ for(i=0; i<n; i++)
+ {
+ scanf("%d",&a[i]);
+ }
+ for(i=0; i<n; i++)
+ {
+ for(j=0; j<n-1; j++)
+ {
+ if(a[j]>a[j+1])
+ {
+ temp=a[j];
+ a[j]=a[j+1];
+ a[j+1]=temp;
+ }
+ }
+ }
+ printf("Descending Order: ");
+ for(i= n-1 ; i>=0 ; i--)
+ {
+ printf("%d ",a[i]);
+ }
+}
